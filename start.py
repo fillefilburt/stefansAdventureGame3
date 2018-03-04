1db1560b0857c260fcd0ffd8b206799783e31868
fillefilburt <steffe.t@gmail.com>
2018-03-04 20:07:15 +0100

    plupp

--- start.py
+++ start.py
@@ -3,10 +3,47 @@ johannes = "mentor"
 #print("hello world "+johannes)
 
 #startmeny typ
+import time
 
 
-name = input('Please tell me you name')
 
+print('Welcome to Stefan\'s Adventure Game! It\'s a really good game.')
+time.sleep(1)
+print('Please set the name, age and gender of your character.')
+time.sleep(2)
+
+name = input('First, tell me you name')
+if name != 'Stig':
+    print(name+'? What a silly name! Let us call you Stig instead. It\'s a good name.')
+    name='Stig'
+else:
+    print('Excellent choice!')
+time.sleep(1)
+
+
+age = 0
+while age < 8 or age > 12:
+    age = int(input('What is you age?'))
+    if age > 12:
+        print('That\'s too old! '+str(age)+'? Stig is just a little kid for fucks sake!')
+    elif age < 8:
+        print('C\'mon, Stig isn\'t that little.')
+    else:
+        print('Yeah, that sounds about right. But who knows? Stig is a kid and no one really knows how old he is, least of all himself.')
+time.sleep(2)
+
+gender = ''
+while gender != 'boy':
+    gender = input('So, last but not least, are you a boy, a girl or other?')
+    if gender not in ['boy', 'girl', 'other']:
+        print('Check your spelling mate.')
+    elif gender == 'other':
+        print('I was being polite. Other is not a gender.')
+    elif gender == 'girl':
+        print('C\'mon dude, does Stig sound like a girl\'s name to you?')
+    else:
+        print('You sure are! Because you have a penis!')
+time.sleep(1)
 
 
 def play():
@@ -14,17 +51,6 @@ def play():
     while alive:
         print("you will die "+name)
         alive = False
-
-
-
-
-
-
-
-print('kuk')
-
-
-
 
 
 
