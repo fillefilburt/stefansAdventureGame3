4d251369778bed3b118941ed37eb051a44408fa9
fillefilburt <steffe.t@gmail.com>
2018-03-04 17:30:39 +0100

    startade hemma, version 3. johannes lär mig

---
+++ start.py
@@ -0,0 +1,37 @@
+johannes = "mentor"
+
+#print("hello world "+johannes)
+
+#startmeny typ
+
+
+name = input('Please tell me you name')
+
+
+
+def play():
+    alive = True
+    while alive:
+        print("you will die "+name)
+        alive = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    play()
